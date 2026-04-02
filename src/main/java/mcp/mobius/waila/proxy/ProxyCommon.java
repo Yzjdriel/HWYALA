4e23293560f45a3ca082495ab0e665c2505acb82
Zid <lymanrj@gmail.com>
2026-04-02 07:29:46 -0400

    Update ProxyCommon.java
    Implemented TigerWalts' fix for Issue#167

--- src/main/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/main/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -61,11 +61,14 @@ public class ProxyCommon implements IProxy {
         Waila.LOGGER.info("Registering plugin at {}", PluginCore.class.getCanonicalName());
         // Register the rest
         List<Map.Entry<Class<?>, IWailaPlugin>> sortedPlugins = Lists.newArrayList(plugins.entrySet());
-        sortedPlugins.sort((o1, o2) -> {
-            if (o1.getKey().getCanonicalName().startsWith("mcp.mobius.waila"))
+                sortedPlugins.sort((o1, o2) -> {
+            if (o1.getKey().getCanonicalName().startsWith("mcp.mobius.waila") && !o2.getKey().getCanonicalName().startsWith("mcp.mobius.waila")) {
                 return -1;
-
-            return o1.getKey().getCanonicalName().compareToIgnoreCase(o2.getKey().getCanonicalName());
+            } else if (!o1.getKey().getCanonicalName().startsWith("mcp.mobius.waila") && o2.getKey().getCanonicalName().startsWith("mcp.mobius.waila")) {
+                return 1;
+            } else {
+                return o1.getKey().getCanonicalName().compareToIgnoreCase(o2.getKey().getCanonicalName());
+            }
         });
         for (Map.Entry<Class<?>, IWailaPlugin> plugin : sortedPlugins) {
             Waila.LOGGER.info("Registering plugin at {}", plugin.getKey().getCanonicalName());
